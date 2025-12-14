62e6c6c60d4e56c5f4e4bd093ee0134bb51ddcfb
unknown <curry2478@naver.com>
2025-12-14 18:56:56 +0900

    Implement Search History and Page Transitions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { useLayoutEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import SignIn from './pages/SignIn';
@@ -19,47 +20,73 @@ function ProtectedRoute({ children }) {
   return children;
 }
 
+// Scroll To Top component to fix scroll position on route change
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+function AppContent() {
+  const location = useLocation();
+
+  return (
+    <>
+      <ScrollToTop />
+      <Header />
+      <div className="page-transition-wrapper">
+        <div key={location.pathname} className="page-content fade-in">
+          <Routes location={location}>
+            <Route path="/signin" element={<SignIn />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/popular"
+              element={
+                <ProtectedRoute>
+                  <Popular />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/search"
+              element={
+                <ProtectedRoute>
+                  <Search />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/wishlist"
+              element={
+                <ProtectedRoute>
+                  <Wishlist />
+                </ProtectedRoute>
+              }
+            />
+            {/* 존재하지 않는 경로는 홈으로 리다이렉트 */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/signin" element={<SignIn />} />
-        <Route 
-          path="/" 
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/popular" 
-          element={
-            <ProtectedRoute>
-              <Popular />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/search" 
-          element={
-            <ProtectedRoute>
-              <Search />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/wishlist" 
-          element={
-            <ProtectedRoute>
-              <Wishlist />
-            </ProtectedRoute>
-          } 
-        />
-        {/* 존재하지 않는 경로는 홈으로 리다이렉트 */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <AppContent />
     </BrowserRouter>
   );
 }
